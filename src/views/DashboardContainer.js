a4884a2f7c2d68303d6bbe4f1e53b476a7880409
wordyallen <joe.narvaez@gmil.com>
2016-04-16 11:30:51 -0400

    merged conflicts

--- src/views/DashboardContainer.js
+++ src/views/DashboardContainer.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-<<<<<<< HEAD
 import AdminForm from '../components/AdminForm'
-=======
 import { Link } from 'react-router'
 import AdminForm from './AdminFormContainer';
->>>>>>> e4549c4c3a5a68f0812ad07e4533e8ccdf44b73d
 
 const mapStateToProps = (state) => ({counter: state.counter })
 const mapDispatchToProps= (dispatch) => ({
